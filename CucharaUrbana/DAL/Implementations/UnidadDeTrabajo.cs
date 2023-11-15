f4fadd8e2ddf18631a6e29cc2b521e35aca18ee0
LeoSolMen <lsolis10885@ufide.ac.cr>
2023-11-14 18:12:08 -0600

    CRUD Factura
    CRUD Factura

---
+++ CucharaUrbana/DAL/Implementations/UnidadDeTrabajo.cs
@@ -0,0 +1,44 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class UnidadDeTrabajo : IUnidadDeTrabajo
+    {
+        public IFacturaDAL _facturaDAL { get; }
+
+        private readonly CucharaUrbanaContext _context;
+
+        public UnidadDeTrabajo(CucharaUrbanaContext context,
+                                IFacturaDAL facturaDAL)
+        {
+            _context = context;
+            _facturaDAL = facturaDAL;
+        
+        }
+
+        public bool Complete()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
