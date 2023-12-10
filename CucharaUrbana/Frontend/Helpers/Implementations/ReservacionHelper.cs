b7f8a5f8dd81346c265ae43334b1b49cedd52a17
LeoSolMen <lsolis10885@ufide.ac.cr>
2023-12-10 03:05:46 -0600

    Backend completado
    Backend completado
    Vistas crear e index agregadas

---
+++ CucharaUrbana/Frontend/Helpers/Implementations/ReservacionHelper.cs
@@ -0,0 +1,86 @@
+﻿using Frontend.Helpers.Interfaces;
+using Frontend.Models;
+using Newtonsoft.Json;
+
+namespace Frontend.Helpers.Implementations
+{
+    public class ReservacionHelper : IReservacionHelper
+    {
+
+        IServiceRepository _repository;
+
+        public ReservacionHelper(IServiceRepository serviceRepository)
+        {
+            _repository = serviceRepository;
+        }
+
+        public ReservacionViewModel AddReservacion(ReservacionViewModel reservacionViewModel)
+        {
+
+            ReservacionViewModel reservacion = new ReservacionViewModel();
+            HttpResponseMessage responseMessage = _repository.PostResponse("api/Reservacion", reservacionViewModel);
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                reservacion = JsonConvert.DeserializeObject<ReservacionViewModel>(content);
+            }
+
+            return reservacion;
+        }
+
+        public void DeleteReservacion(int id)
+        {
+
+            HttpResponseMessage responseMessage = _repository.DeleteResponse("api/Reservacion/" + id.ToString());
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            }
+
+
+        }
+
+        public ReservacionViewModel EditReservacion(ReservacionViewModel reservacionViewModel)
+        {
+
+            ReservacionViewModel reservacion = new ReservacionViewModel();
+            HttpResponseMessage responseMessage = _repository.PutResponse("api/Reservacion", reservacionViewModel);
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                reservacion = JsonConvert.DeserializeObject<ReservacionViewModel>(content);
+            }
+
+            return reservacion;
+        }
+
+        public List<ReservacionViewModel> GetAll()
+        {
+
+            List<ReservacionViewModel> lista = new List<ReservacionViewModel>();
+
+            HttpResponseMessage responseMessage = _repository.GetResponse("api/Reservacion");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<ReservacionViewModel>>(content);
+            }
+
+            return lista;
+        }
+
+        public ReservacionViewModel GetById(int id)
+        {
+            ReservacionViewModel reservacion = new ReservacionViewModel();
+            HttpResponseMessage responseMessage = _repository.GetResponse("api/Reservacion/" + id.ToString());
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                reservacion = JsonConvert.DeserializeObject<ReservacionViewModel>(content);
+            }
+
+            return reservacion;
+        }
+    }
+}
---
+++ CucharaUrbana/Frontend/Helpers/Interfaces/IReservacionHelper.cs
@@ -0,0 +1,15 @@
+﻿using Frontend.Models;
+
+namespace Frontend.Helpers.Interfaces
+{
+    public interface IReservacionHelper
+    {
+        List<ReservacionViewModel> GetAll();
+        ReservacionViewModel GetById(int id);
+        ReservacionViewModel AddReservacion(ReservacionViewModel reservacionViewModel);
+        ReservacionViewModel EditReservacion(ReservacionViewModel reservacionViewModel);
+
+        void DeleteReservacion(int id);
+
+    }
+}
