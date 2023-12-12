8933d623812246dd60a992df727606975c16fb30
sebasrodrip <sebasrodrip1208@gmail.com>
2023-12-11 18:55:30 -0600

    Adicion de Carrito al Backend

---
+++ CucharaUrbana/Backend/Models/CarritoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Models
+{
+    public class CarritoModel
+    {
+        public int CarritoId { get; set; }
+        public int UsuarioId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
---
+++ CucharaUrbana/Backend/Services/Implementations/CarritoService.cs
@@ -0,0 +1,78 @@
+﻿using Backend.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace Backend.Services.Implementations
+{
+    public class CarritoService : ICarritoService
+    {
+
+        IUnidadDeTrabajo _unidadDeTrabajo;
+        public CarritoService(IUnidadDeTrabajo unidadDeTrabajo)
+        {
+            _unidadDeTrabajo = unidadDeTrabajo;
+        }
+
+
+        public Task<bool> Add(Carrito carrito)
+        {
+            try
+            {
+                _unidadDeTrabajo._carritoDAL.Add(carrito);
+                _unidadDeTrabajo.Complete();
+                return Task.FromResult(true);
+            }
+            catch (Exception)
+            {
+
+                return Task.FromResult(false);
+            }
+
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            try
+            {
+                Carrito carrito = new Carrito { CarritoId = id };
+
+                _unidadDeTrabajo._carritoDAL.Remove(carrito);
+                _unidadDeTrabajo.Complete();
+                return Task.FromResult(true);
+
+            }
+            catch (Exception)
+            {
+
+                return Task.FromResult(false); ;
+            }
+        }
+
+        public async Task<Carrito> GetById(int id)
+        {
+            Carrito carrito = _unidadDeTrabajo._carritoDAL.Get(id);
+            return carrito;
+        }
+
+        public async Task<IEnumerable<Carrito>> GetCarritos()
+        {
+                IEnumerable<Carrito> carritos = await _unidadDeTrabajo._carritoDAL.GetAll();
+                return carritos;
+        }
+
+        public Task<bool> Update(Carrito carrito)
+        {
+            try
+            {
+                _unidadDeTrabajo._carritoDAL.Update(carrito);
+                _unidadDeTrabajo.Complete();
+                return Task.FromResult(true);
+            }
+            catch (Exception)
+            {
+
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
