6b69f9e7fc4d11d36234d3fe2fc47ef503ce648e
sebasrodrip <sebasrodrip1208@gmail.com>
2023-12-12 19:19:22 -0600

    Update Database

--- CucharaUrbanaDB.sql
+++ CucharaUrbanaDB.sql
@@ -2,13 +2,13 @@ USE CucharaUrbana;
 
 -- Crear la tabla Rol
 CREATE TABLE Rol (
-    RolID INT PRIMARY KEY,
+    RolID INT IDENTITY (1,1) PRIMARY KEY,
     NombreRol NVARCHAR(50) NOT NULL
 );
 
 -- Crear tabla Usuarios
 CREATE TABLE Usuario (
-    UsuarioID INT PRIMARY KEY,
+    UsuarioID INT IDENTITY (1,1) PRIMARY KEY,
     Nombre NVARCHAR(50) NOT NULL,
     Apellido NVARCHAR(50) NOT NULL,
     CorreoElectronico NVARCHAR(100) UNIQUE NOT NULL,
@@ -20,7 +20,7 @@ SELECT * FROM Usuario
 -- Crear Tabla Categorias
 
 CREATE TABLE Categoria (
-	CategoriaID INT PRIMARY KEY,
+	CategoriaID INT IDENTITY (1,1) PRIMARY KEY,
 	Nombre VARCHAR (30),
 );
 
@@ -28,7 +28,7 @@ CREATE TABLE Categoria (
 -- Creacion de la tabla productos
 
 CREATE TABLE Producto (
-	ProductoID INT PRIMARY KEY,
+	ProductoID INT  IDENTITY (1,1) PRIMARY KEY,
 	Nombre VARCHAR(50) NOT NULL,
 	Descripcion VARCHAR (100),
 	CategoriaID INT,
@@ -42,7 +42,7 @@ ALTER TABLE Producto
 ALTER COLUMN Descripcion VARCHAR(500);
 
 CREATE TABLE Carrito (
-    CarritoID INT PRIMARY KEY,
+    CarritoID INT IDENTITY (1,1) PRIMARY KEY,
     UsuarioID INT NOT NULL,
     ProductoID INT NOT NULL,
     Cantidad INT NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE Carrito (
 -- Creación de tabla Pedidos:
 
 CREATE TABLE Pedidos (	
-	PedidoID INT PRIMARY KEY,
+	PedidoID INT IDENTITY (1,1) PRIMARY KEY,
 	FechaPedido  DATETIME NOT NULL,  	
 	MesaPedido   INT NOT NULL,	
 	UsuarioID    INT, --llave foranea del id cliente 
@@ -68,7 +68,7 @@ CREATE TABLE Pedidos (
 --Creación de tabla Reservaciones:
 
 CREATE TABLE Reservacion (
-	ReservacionID      INT PRIMARY KEY,
+	ReservacionID      INT IDENTITY (1,1) PRIMARY KEY,
 	UsuarioID          INT,
 	FechaReservacion   DATETIME NOT NULL,
 	MesaReservacion    INT NOT NULL,
@@ -81,13 +81,13 @@ CREATE TABLE Reservacion (
 
 	-- Creación de la tabla temporal "TipoPago" en caso de requerirse
 CREATE TABLE TipoPago  (
-    MetodoPagoID INT PRIMARY KEY,
+    MetodoPagoID INT IDENTITY (1,1) PRIMARY KEY,
     TipoPago VARCHAR(50) NOT NULL
 );
 
 	-- Creación de la tabla "Factura"
 CREATE TABLE Factura (
-    FacturaID INT PRIMARY KEY,
+    FacturaID INT IDENTITY (1,1) PRIMARY KEY,
     FechaFactura DATE NOT NULL,
     UsuarioID INT NOT NULL, --Llave foranea de la tabla Clientes para mostrar el id del cliente 
     Total DECIMAL(10, 2) NOT NULL,
@@ -101,7 +101,7 @@ CREATE TABLE Factura (
 
 -- Creación de la tabla "DetalleFactura"
 CREATE TABLE DetalleFactura (
-    DetalleID INT PRIMARY KEY,
+    DetalleID INT IDENTITY (1,1) PRIMARY KEY,
     FacturaID INT,
     ProductoID INT NOT NULL,
     Cantidad INT NOT NULL,
@@ -122,31 +122,33 @@ DROP COLUMN PrecioUnitario;
 -- Inserts
 
 -- Insertar datos en la tabla Rol
-INSERT INTO Rol (RolID, NombreRol) VALUES (1, 'Administrador');
-INSERT INTO Rol (RolID, NombreRol) VALUES (2, 'Usuario');
+INSERT INTO Rol (NombreRol) VALUES ('Administrador');
+INSERT INTO Rol (NombreRol) VALUES ('Usuario');
 
 -- Insertar datos en la tabla Usuario
-INSERT INTO Usuario (UsuarioID, Nombre, Apellido, CorreoElectronico, RolID) VALUES (1, 'Juan', 'Perez', 'juan@example.com', 1);
-INSERT INTO Usuario (UsuarioID, Nombre, Apellido, CorreoElectronico, RolID) VALUES (2, 'Maria', 'Lopez', 'maria@example.com', 2);
+INSERT INTO Usuario (Nombre, Apellido, CorreoElectronico, RolID) VALUES ( 'Juan', 'Perez', 'juan@example.com', 1);
+INSERT INTO Usuario (Nombre, Apellido, CorreoElectronico, RolID) VALUES ('Maria', 'Lopez', 'maria@example.com', 2);
 
 -- Insertar datos en la tabla Categoria
-INSERT INTO Categoria (CategoriaID, Nombre) VALUES (1, 'Comida');
-INSERT INTO Categoria (CategoriaID, Nombre) VALUES (2, 'Bebida');
+INSERT INTO Categoria ( Nombre) VALUES ('Comida');
+INSERT INTO Categoria ( Nombre) VALUES ('Bebida');
 
 -- Insertar datos en la tabla Producto
-INSERT INTO Producto (ProductoID, Nombre, Descripcion, CategoriaID, Precio) VALUES (1, 'Melt Callejero', 
+INSERT INTO Producto (Nombre, Descripcion, CategoriaID, Precio) VALUES ( 'Melt Callejero', 
 'Pan de molde, 2 smash de carne, 2 rebanadas de cheddar, 2 rebanadas de queso americano, cebolla lentamente caramelizada, salsa urbana y derretido en mantequilla a la plancha', 1, 4600);
-INSERT INTO Producto (ProductoID,Nombre, Descripcion, CategoriaID, Precio) VALUES (2, 'Urban Classic Doble', 
+INSERT INTO Producto (Nombre, Descripcion, CategoriaID, Precio) VALUES ('Urban Classic Doble', 
 'Pan de papa, 2 smash de carne, 2 rebanadas de queso americano, 1 rebanada de queso cheddar, pepinilos de la casa, cebolla picada, salsa urbana', 1, 4600);
-INSERT INTO Producto (ProductoID,Nombre, Descripcion, CategoriaID, Precio) VALUES (3, 'Urban Classic Triple', 
+INSERT INTO Producto (Nombre, Descripcion, CategoriaID, Precio) VALUES ( 'Urban Classic Triple', 
 'Pan de papa, 3 smash de carne, 2 rebanadas de queso americano, 1 rebanada de queso cheddar, pepinilos de la casa, cebolla picada, salsa urbana', 1, 5300);
-INSERT INTO Producto (ProductoID,Nombre, Descripcion, CategoriaID, Precio) VALUES (4, 'Krunchy Bella', 
+INSERT INTO Producto (Nombre, Descripcion, CategoriaID, Precio) VALUES ( 'Krunchy Bella', 
 'Pan de papa, 2 smash de carne, tocineta crujiente, cebolla crujiente, 1 rebanada de queso americano, 1 reabanada de queso cheddar y salsa urbana', 1, 4900);
-INSERT INTO Producto (ProductoID,Nombre, Descripcion, CategoriaID, Precio) VALUES (5, 'Juicy Luchi', 
+INSERT INTO Producto (Nombre, Descripcion, CategoriaID, Precio) VALUES ( 'Juicy Luchi', 
 'Pan de papa, 2 smash de carne sin vuelta, en medio 1 rebanada de cheddar y 2 de queso americano, pepinillos, cebolla picada, kétchup y mostaza', 1, 4650);
-INSERT INTO Producto (ProductoID,Nombre, CategoriaID, Precio) VALUES (6, 'Extra papas fritas',  1, 1000);
+INSERT INTO Producto (Nombre, CategoriaID, Precio) VALUES ('Extra papas fritas',  1, 1000);
 
+SELECT * FROM Producto
 
+DELETE FROM Producto WHERE ProductoId = 6
 -- Insertar datos en la tabla TipoPago
-INSERT INTO TipoPago (MetodoPagoID, TipoPago) VALUES (1, 'Efectivo');
-INSERT INTO TipoPago (MetodoPagoID, TipoPago) VALUES (2, 'Tarjeta');
+INSERT INTO TipoPago (TipoPago) VALUES ('Efectivo');
+INSERT INTO TipoPago (TipoPago) VALUES ('Tarjeta');
