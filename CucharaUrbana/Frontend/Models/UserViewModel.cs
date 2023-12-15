6e44f4c6e51d438f338af4274ed38f42eaeb11df
sebasrodrip <sebasrodrip1208@gmail.com>
2023-12-15 11:40:06 -0600

    correciones

--- CucharaUrbana/Frontend/Models/UserViewModel.cs
+++ CucharaUrbana/Frontend/Models/UserViewModel.cs
@@ -4,9 +4,11 @@ namespace Frontend.Models
 {
     public class UserViewModel
     {
+        [Required]
         public string UserName { get; set; }
         public string ? Email { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         public bool RememberLogin { get; set; }
